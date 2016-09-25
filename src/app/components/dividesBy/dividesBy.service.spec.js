48012098bdf068485e7a2c0cb92c0504a9ab088c
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-09-25 04:07:20 +0200

    added test for dividesBy service

--- src/app/components/dividesBy/dividesBy.service.spec.js
+++ src/app/components/dividesBy/dividesBy.service.spec.js
@@ -1,18 +1,26 @@
-// (function(){
-// 	'use strict';
+(function(){
+  'use strict';
 
-// 	describe('service socialLinks', function(){
-// 		var socialLinks;
+  describe('service dividesBy', function(){
+    var dividesBy;
 
-// 		beforeEach(module('angularPortfolio'));
-// 		beforeEach(inject(function(_socialLinks_){
-// 			socialLinks = _socialLinks_;
-// 		}));
+    beforeEach(module('angularPortfolio'));
+    beforeEach(inject(function(_dividesBy_){
+      dividesBy = _dividesBy_;
+    }));
 
-// 		it('should return an list which contains 3 objects with iconUrl properties', function(){
-// 			expect(socialLinks).toEqual(jasmine.any(Array));
-// 			expect(socialLinks.length).toEqual(3);
-// 			expect(socialLinks[0].iconUrl).toBeDefined();
-// 		});
-// 	});
-// })()
+    it('should return true if second parameter can be divided by first', function(){
+      expect(dividesBy(3, 6)).toEqual(true);
+      expect(dividesBy(1, 6)).toEqual(true);
+    });
+    it('should return false if second parameter cant be divided by first', function(){
+      expect(dividesBy(4, 6)).toBeFalsy();
+      expect(dividesBy(6, 1)).toBeFalsy();
+    })
+    it('should know what to do with a 0 zero', function(){
+      expect(dividesBy(1, 0)).toBeTruthy();
+      expect(dividesBy(0, 1)).toBeFalsy();
+      expect(dividesBy(0, 0)).toBeFalsy();
+    })
+  });
+})()
